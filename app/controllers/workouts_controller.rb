95ed69a28c4e363b7800820900396cca3ac80ca1
Miguel Oliveira <miguel.gazela@gmail.com>
2015-10-15 21:26:05 +0100

    Fixed bug

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,7 +1,7 @@
 class WorkoutsController < ApplicationController
 
-	before_action :require_user, only: [:week_workouts]
-	before_action :require_account_management_rights, only: [:new, :create, :change_training_state]
+	before_action :require_user, only: [:week_workouts, :change_training_state]
+	before_action :require_account_management_rights, only: [:new, :create]
 	before_action :clear_gon, only: [:show, :week_workouts, :new, :create]
 
 	def show
